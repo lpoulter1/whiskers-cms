3528397849b24860a78ab70ce2101e90ecddeede
K-velvet-P <123256886+K-velvet-P@users.noreply.github.com>
2024-04-02 20:00:51 +0100

    installed shadcn and testermonials swiper

---
+++ src/components/dogCarousel.jsx
@@ -0,0 +1,73 @@
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import { Card, CardContent } from "@/components/ui/card";
+import { reviews } from "src/data/testimonials.astro";
+
+console.log(reviews);
+
+export default function DogCarousel() {
+  return (
+    <Carousel className="w-full max-w-4xl">
+      <CarouselContent>
+        {reviews.map((review, index) => {
+          // Get the next review in the array
+          const nextReview = reviews[index + 1];
+          
+          return (
+            <CarouselItem key={index}>
+              <div className="flex border-2 border-blue-500 gap-5">
+                <div className="w-1/2 p-1 border-2 border-red-500 ">
+                  <Card>
+                    <CardContent className="flex flex-col aspect-square items-center justify-center p-6 ">
+                      <div class="border-2 flex flex-col gap-5 m-2 items-center p-5">
+                        <p>{index}</p>
+                        <p class="text-center">"{review.text}"</p>
+                        <div class="flex flex-col text-center items-center">
+                          <img
+                            src={review.image}
+                            alt={review.imageAlt}
+                            className="rounded-full h-20 w-20 object-cover"
+                          />
+                          <h4>{review.name}</h4>
+                          <h5>{review.dogBreed}</h5>
+                        </div>
+                      </div>
+                    </CardContent>
+                  </Card>
+                </div>
+                {nextReview && (
+                  <div className="w-1/2 p-1 border-2 border-green-500 ">
+                    <Card>
+                      <CardContent className="flex flex-col aspect-square items-center justify-center p-6">
+                        <div class="border-2 flex flex-col gap-5 m-2 items-center p-5">
+                          <p>{index}</p>
+                          <p class="text-center">"{nextReview.text}"</p>
+                          <div class="flex flex-col text-center items-center">
+                            <img
+                              src={nextReview.image}
+                              alt={nextReview.imageAlt}
+                              className="rounded-full h-20 w-20 object-cover"
+                            />
+                            <h4>{nextReview.name}</h4>
+                            <h5>{nextReview.dogBreed}</h5>
+                          </div>
+                        </div>
+                      </CardContent>
+                    </Card>
+                  </div>
+                )}
+              </div>
+            </CarouselItem>
+          );
+        })}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  );
+}
