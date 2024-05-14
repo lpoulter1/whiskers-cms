45ba7f22a45831dee1083e99e4394428d894e3ec
kepea1 <k-velvet-p@protonmail.com>
2024-05-14 12:54:15 +0100

    made icon green and added hover orange icons

--- src/components/navbar-shadcn.jsx
+++ src/components/navbar-shadcn.jsx
@@ -12,14 +12,14 @@ import { Image } from "astro:assets";
 
 export default function NavbarShadcn() {
   return (
-    <nav className="flex items-center bg-white border-b-2 text-green-700 ">
+    <nav className="flex items-center bg-white border-b-2 text-green-700">
       <div className="basis-full p-3 ">
         <NavigationMenu>
           <NavigationMenuList>
             <NavigationMenuItem className="transition ease-in-out hover:-translate-y-0.5 hover:scale-110 hover:text-orange-600">
               <NavigationMenuLink href="#home">
-                <img src="/logo_text_green.jpg" className="size-2/5 inline-block group-hover:hidden" alt="waggles &whiskers" />      
-                <img src="/logo_text_orange.jpg" className="size-2/5 hidden group-hover:inline-block" alt="waggles &whiskers" />      
+                 <img src="/logo_text.png" className="size-2/5 inline-block group-hover:hidden" alt="waggles &whiskers" />      
+                <img src="/logo_text_orange.png" className="size-2/5 hidden group-hover:inline-block" alt="waggles &whiskers" />      
                         </NavigationMenuLink>
             </NavigationMenuItem>
           </NavigationMenuList>
@@ -28,9 +28,10 @@ export default function NavbarShadcn() {
       <div className="basis-full flex justify-center	">
         <img
           className="hidden m-1 p-1 h-20 md:block "
-          src="/logo_noText_green.jpg"
+          src="/Waggles&Whiskers-no-text.png"
           alt="Waggles & Whiskers"
         />
+      
       </div>
       <div className="basis-full flex justify-end p-3 font-cursive text-3xl font-bold tracking-wide">
         <NavigationMenu>
