301ff6faccda6bd397c5219a16fc25f4aa61d035
kepea1 <k-velvet-p@protonmail.com>
2024-05-21 12:33:02 +0100

    added logo to mobile view

--- src/components/navbar-shadcn-drop.jsx
+++ src/components/navbar-shadcn-drop.jsx
@@ -10,15 +10,19 @@ import { HamburgerMenuIcon, Cross1Icon } from "@radix-ui/react-icons";
 
 export default function NavbarShadcnDrop() {
   return (
-    <section className="flex gap-3 justify-between items-center bg-white p-2 ">
-      <a href="#home">
-      <img src="/logo_text.png" className="size-2/5" alt="waggles &whiskers" />  
-      </a>
+    <section className="flex gap-3 justify-between items-center bg-white p-2 border-b-2 ">
+      <a href="#home"><img src="/logo_text.png" className="size-2/5 inline-block group-hover:hidden" width="300" height="300" alt="waggles &whiskers" />  </a>
+      <img
+        className="hidden size-20 md:block"
+        src="/logo.jpg"
+        alt="Waggles & Whiskers"
+      />
+
       <DropdownMenu>
         <DropdownMenuTrigger className="open:hidden">
-          <HamburgerMenuIcon className="size-8 " />
+          <HamburgerMenuIcon className="size-8" />
         </DropdownMenuTrigger>
-        <DropdownMenuContent className=" text-4xl">
+        <DropdownMenuContent>
           <DropdownMenuItem>
             <a href="#services">Services & Prices</a>
           </DropdownMenuItem>
