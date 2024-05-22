f2397241d8790ea9d7a3cdc68b18498c3e467d16
kepea1 <123256886+kepea1@users.noreply.github.com>
2024-05-22 08:54:35 +0100

    adjusted photos and stlying

--- src/components/nav-menu.jsx
+++ src/components/nav-menu.jsx
@@ -1,62 +1,46 @@
 import {
-    NavigationMenu,
-    NavigationMenuContent,
-    NavigationMenuIndicator,
-    NavigationMenuItem,
-    NavigationMenuLink,
-    NavigationMenuList,
-    NavigationMenuTrigger,
-    NavigationMenuViewport,
-  } from "@/components/ui/navigation-menu"
-  import { HamburgerMenuIcon } from "@radix-ui/react-icons";
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "@/components/ui/navigation-menu";
+import { HamburgerMenuIcon } from "@radix-ui/react-icons";
 
-  
-  
-  
-  export default function NavMenu() {
-    return (
-        <section className="flex gap-3 justify-between items-center bg-white p-2 border-b-2 ">
-       
-        
+export default function NavMenu() {
+  return (
+    <section className="flex gap-3 justify-between items-center bg-white p-2 border-b-2 ">
+      <a href="#profile ">
+        <img
+          src="/logo_text.png"
+          className="size-2/5 inline-block group-hover:hidden"
+          width="300"
+          height="300"
+          alt="waggles &whiskers"
+        />{" "}
+      </a>
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>
+              <HamburgerMenuIcon className="size-8" />
+            </NavigationMenuTrigger>
 
-          <a href="#profile ">
-          <img src="/logo_text.png" className="size-2/5 inline-block group-hover:hidden" width="300" height="300" alt="waggles &whiskers" />  </a>
-          <NavigationMenu>
-            <NavigationMenuList>
-             <NavigationMenuItem>
-             <NavigationMenuTrigger>
-            <HamburgerMenuIcon className="size-8" />
-
-         </NavigationMenuTrigger>
-
-         <NavigationMenuContent className="flex flex-col gap-6 text-lg border-2 p-3"   >
-        
-        <NavigationMenuLink>  
-             <a href="#services">Services & Prices</a></NavigationMenuLink>
-        <NavigationMenuLink>
-        <a href="#dog-gallery">Gallery</a>
-
-
-        </NavigationMenuLink>
-        <NavigationMenuLink>
-        <a href="#testimonials">Testimonials</a>
-
-        
-        </NavigationMenuLink>
-      </NavigationMenuContent>
-
-    </NavigationMenuItem>
-  </NavigationMenuList>
-</NavigationMenu>
-
-
-
-
-
-
-</section>
-
-    );
-
-
-  };
+            <NavigationMenuContent className="flex flex-col gap-6 text-lg border-2 p-3 ">
+              <NavigationMenuLink>
+                <a href="#services" className=" hover:text-orange-600">Services & Prices</a>
+              </NavigationMenuLink>
+              <NavigationMenuLink>
+                <a href="#dog-gallery" className=" hover:text-orange-600">Gallery</a>
+              </NavigationMenuLink>
+              <NavigationMenuLink>
+                <a href="#testimonials" className=" hover:text-orange-600">Testimonials</a>
+              </NavigationMenuLink>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </section>
+  );
+}
--- src/components/navbar-shadcn.jsx
+++ src/components/navbar-shadcn.jsx
@@ -6,7 +6,6 @@ import {
 } from "@/components/ui/navigation-menu";
 import { Image } from "astro:assets";
 
-
 export default function NavbarShadcn() {
   return (
     <nav className="flex items-center bg-white border-b-2 border-green-700 border-opacity-35 text-neutral-900">
@@ -15,27 +14,33 @@ export default function NavbarShadcn() {
           <NavigationMenuList>
             <NavigationMenuItem className="transition ease-in-out hover:-translate-y-0.5 hover:scale-110 hover:text-orange-600">
               <NavigationMenuLink href="#top">
-
-                 <img src="/logo_text.png" className="size-2/5 inline-block group-hover:hidden" width="300" height="300" alt="waggles &whiskers" />      
-                <img src="/logo_text_orange.png" className="size-2/5 hidden group-hover:inline-block" width="300" height="300" alt="waggles &whiskers" />      
-
-                        </NavigationMenuLink>
+                <img
+                  src="/logo_text.png"
+                  className="size-2/5 inline-block group-hover:hidden"
+                  width="300"
+                  height="300"
+                  alt="waggles &whiskers"
+                />
+                <img
+                  src="/logo_text_orange.png"
+                  className="size-2/5 hidden group-hover:inline-block"
+                  width="300"
+                  height="300"
+                  alt="waggles &whiskers"
+                />
+              </NavigationMenuLink>
             </NavigationMenuItem>
           </NavigationMenuList>
         </NavigationMenu>
       </div>
       <div className="basis-full flex justify-center	">
-   
         <img
-
           className="hidden m-1 pb-2 b-2 h-20 md:block size-2/6"
           src="/Waggles&Whiskers-no-text.png"
-
           alt="Waggles & Whiskers"
           width="150"
           height="150"
         />
-      
       </div>
       <div className="basis-full flex justify-end p-3 font-cursive text-3xl font-bold tracking-wide">
         <NavigationMenu>
